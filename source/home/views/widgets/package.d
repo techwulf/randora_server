01af08582708989db6b9c1c383568f057236661f
Master-Foo <joshka.randora@gmail.com>
2016-02-09 17:24:58 -0700

    Add UsaMap

--- source/home/views/widgets/package.d
+++ source/home/views/widgets/package.d
@@ -4,3 +4,4 @@ package import home.views.widgets.dashboard.dashboard;
 package import home.views.widgets.home_menu.home_menu;
 package import home.views.widgets.task.tasks;
 package import home.views.widgets.recent_postings.recent_postings;
+package import home.views.widgets.usa_map.usa_map;
--- source/home/views/widgets/usa_map.d
+++ source/home/views/widgets/usa_map.d
@@ -1,32 +1,38 @@
-module widget.views.widgets.usa_map.usa_map;
+module home.views.widgets.usa_map.usa_map;
 
-import widget;
+import home;
 
 class WidgetUsaMap : DivElement{
+	string title = "";
 	this(){
 		super();
-		init();
+		this.tag.attr["class"] = "tile";
+		this.title = "Live Visits";
+		this.init();
 	}
 
-	void init(){}
-	/+
-	override string init(){
-		this.value = "";
-		this.value ~= "
-<div class='tile'>
-	<h2 class='tile-title'>Live Visits</h2>
-	<div class='tile-config dropdown'>
-		<a data-toggle='dropdown' href='' class='tile-menu'></a>
-		<ul class='dropdown-menu pull-right text-right'>
-			<li><a href=''>Refresh</a></li>
-			<li><a href=''>Settings</a></li>
-		</ul>
-	</div>
-	<div id='usa-map'></div>
-</div>
-		";
-		return this.value;
+	class TileTitle : H2Element{
+		this(string title){
+			super();
+			this.tag.attr["class"] = "tile-title";
+			this ~= new Text(title);
+		}
 	}
-	+/
+
+	void init(){
+		this ~= new TileTitle(this.title);
+		//this ~= new TileConfig();
+	}
 }
 
+/+
+<h2 class='tile-title'>Live Visits</h2>
+<div class='tile-config dropdown'>
+	<a data-toggle='dropdown' href='' class='tile-menu'></a>
+	<ul class='dropdown-menu pull-right text-right'>
+		<li><a href=''>Refresh</a></li>
+		<li><a href=''>Settings</a></li>
+	</ul>
+</div>
+<div id='usa-map'></div>
++/
