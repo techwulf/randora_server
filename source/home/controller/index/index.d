ae6353b74f8aff3c2ecf6b930b7948e811034e4f
Master-Foo <joshka.randora@gmail.com>
2016-03-11 17:41:33 -0700

    Use DeepMagic library

--- source/home/controller/index/index.d
+++ source/home/controller/index/index.d
@@ -5,11 +5,20 @@ import home;
 final class IndexController : HomeControllerPrototype{
 	LayoutElement view = null;
 
+	string[][] dashboard = [
+		["img/shortcuts/money.png",			"Purchases"],
+		["img/shortcuts/twitter.png",		"Tweets"],
+		["img/shortcuts/calendar.png",		"Calendar"],
+		["img/shortcuts/stats.png",			"Statistics"],
+		["img/shortcuts/connections.png",	"Connection"],
+		["img/shortcuts/reports.png",		"Reports"]
+	];
+
 	this(){
 		super();
 		this.name = "index";
 
-		this.view = new IndexLayout();
+		this.view = new IndexLayout(this.dashboard);
 
 		this.router.registerWebInterface(this, this.web_interface_settings);
 	}
--- source/home/views/widgets/dashboard.d
+++ source/home/views/widgets/dashboard.d
@@ -3,53 +3,41 @@ module home.views.widgets.dashboard.dashboard;
 import home;
 
 class DashboardWidget : DivElement{
-	string _title = "";
+	string _title = "Dashboard";
+	string[][] _datastructure = null;
 
-	string[][] datastructures = [
-		["img/shortcuts/money.png", "Purchases"],
-		["img/shortcuts/twitter.png", "Tweets"],
-		["img/shortcuts/calendar.png", "Calendar"],
-		["img/shortcuts/stats.png", "Statistics"],
-		["img/shortcuts/connections.png", "Connection"],
-		["img/shortcuts/reports.png", "Reports"]
-	];
-
-	this(){
+	this(string[][] datastructure){
 		super();
-		this._title = "Dashboard";
+		this._datastructure = datastructure;
 		this.init();
 	}
 
 	override void init(){
-		H4Element title = new H4Element(this._title);
-		title.add_class("page-title");
-
-		this ~= title;
-		this ~= new ShortcutArea(this.datastructures);
+		this ~= new H4Element(
+			Attributes(null, ["page-title"]),
+			this._title
+		);
+		this ~= new ShortcutArea(this._datastructure);
 	}
 
 	class ShortcutArea : DivElement{
-		this(string[][] datastructures){
-			super();
-			this.add_class("block-area shortcut-area");
+		this(string[][] datastructure){
+			super(Attributes(null, ["block-area","shortcut-area"]), "");
 
-			foreach(int key, string[] row; datastructures){
-				ImgElement image = new ImgElement();
-				image.tag.attr["src"] = row[0]; //TODO: add src attribute to Html5 library. Should access like: this.src = image;
-				image.tag.attr["alt"] = row[1]; //TODO: add alt attribute to Html5 library. Should access like: this.alt = "";
-
-				SmallElement title = new SmallElement(row[1]);
-				title.add_class("t-overflow");
-
-				AElement anchor = new AElement([
-					image,
-					title
-				]);
-				anchor
-					.add_class("shortcut")
-					.add_class("tile");
-
-				this ~= anchor;
+			foreach(int key, string[] row; datastructure){
+				this ~= new AElement(
+					Attributes(null, ["shortcut", "tile"]),
+					[
+						new ImgElement(
+							Attributes(null,null,["src" : row[0], "alt" : row[1]]),
+							""
+						),
+						new SmallElement(
+							Attributes(null,["t-overflow"]),
+							row[1]
+						)
+					]
+				);
 			}
 		}
 	}
