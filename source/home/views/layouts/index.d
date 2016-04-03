b5a8f15989c7db71607e26316d359bdcad486bd4
Master-Foo <joshka.randora@gmail.com>
2016-04-02 20:19:21 -0600

    Refactor home index view

--- source/home/views/layouts/index.d
+++ source/home/views/layouts/index.d
@@ -18,52 +18,39 @@ class IndexLayout : LayoutElement{
 		this ~= new QuickstatsWidget(this._datastructure.quickstats);
 		this ~= new WhiterElement();
 
-		this ~= new BlockArea(this._datastructure);
+		this ~= new DivElement(
+			Attributes(null, [new Sass("block-area")]),
+			[
+				new DivElement(
+					Attributes(null, [new BootstrapRow()]),
+					[
+						new DivElement(
+							Attributes(null, [new BootstrapColumn(8)]),
+							[
+								new DivElement(
+									Attributes(null, [new BootstrapRow()]),
+									[
+										new PieChartWidget(this._datastructure.charts),
+										new RecentPostingsWidget(this._datastructure.recent_posting),
+										new TasksWidget(this._datastructure.tasks)
+									]
+								)
+							]
+						),
+						new DivElement(
+							Attributes(null, [new BootstrapColumn(4)]),
+							[
+								new UsaMapWidget(this._datastructure.live_visits),
+								//new DynamicChartWidget(),
+								//new ActivityWidget(),
+								new ClearFixElement()
+							]
+						)
+					]
+				)
+			]
+		);
+
 		this ~= new WhiterElement();
-	}
-
-	class BlockArea : BlockAreaElement{
-		this(HomeIndexDatastructure datastructure){
-			super();
-			this ~= new Row(datastructure);
-		}
-
-		class Row : RowElement{
-			this(HomeIndexDatastructure datastructure){
-				super();
-				this ~= new Col1(datastructure);
-				this ~= new Col2(datastructure);
-				this ~= new ClearFixElement();
-			}
-
-			class Col1 : BootstrapColumn!(DivElement){
-				this(HomeIndexDatastructure datastructure){
-					this.md = 8;
-					super();
-					this ~= new Row(datastructure);
-					this ~= new ClearFixElement();
-				}
-
-				class Row : RowElement{
-					this(HomeIndexDatastructure datastructure){
-						super();
-						this ~= new PieChartWidget(datastructure.charts);
-						this ~= new RecentPostingsWidget(datastructure.recent_posting);
-						this ~= new TasksWidget(datastructure.tasks);
-					}
-				}
-			}
-
-			class Col2 : BootstrapColumn!(DivElement){
-				this(HomeIndexDatastructure datastructure){
-					this.md = 4;
-					super();
-					this ~= new UsaMapWidget(datastructure.live_visits);
-					//this ~= new DynamicChartWidget();
-					//this ~= new ActivityWidget();
-					this ~= new ClearFixElement();
-				}
-			}
-		}
 	}
 }
